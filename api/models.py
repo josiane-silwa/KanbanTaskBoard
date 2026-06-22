1f091b8a908fac8dc6945123c4a15a5384af00ed
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2026-06-22 19:41:57 -0300

    criacao da models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('TODO', 'A Fazer'),
+        ('IN_PROGRESS', 'Em Progresso'),
+        ('DONE', 'Concluído'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('LOW', 'Baixa'),
+        ('MEDIUM', 'Média'),
+        ('HIGH', 'Alta'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='TODO')
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='MEDIUM')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
