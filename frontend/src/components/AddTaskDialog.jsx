7a640f66fe9f5e142e42ab95a8bfa8ce979afd70
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2026-07-05 23:48:00 -0300

    Montagem das chamadas à api e das funcao de crud no frontend

--- frontend/src/components/AddTaskDialog.jsx
+++ frontend/src/components/AddTaskDialog.jsx
@@ -1,10 +1,22 @@
 // src/components/AddTaskDialog.jsx
 import { useState } from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, MenuItem, Stack } from '@mui/material';
-import axios from 'axios';
+import { createTasks } from '../services/api'
+// import axios from 'axios';
 
 export default function AddTaskDialog({ open, handleClose, onTaskAdded }) {
   const [formData, setFormData] = useState({ title: '', description: '', priority: 'MEDIUM', status: 'TODO' });
+
+  // update with Axios
+  const createTasksCards = async (...formData) => {
+    try {
+      await createTasks(  ...formData  );
+      // createTasks.post(formData);
+      console.log('Patched Post:', ...formData);
+    } catch (error) {
+      console.error("Error Post:", error);
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -16,7 +28,8 @@ export default function AddTaskDialog({ open, handleClose, onTaskAdded }) {
     if (!formData.title.trim()) return;
 
     try {
-      await axios.post('http://localhost:8000/api/api/', formData);
+      // await axios.post('http://localhost:8000/api/api/', formData);
+      await createTasksCards(formData);
       setFormData({ title: '', description: '', priority: 'MEDIUM', status: 'TODO' });
       onTaskAdded(); // Dispara o recarregamento do quadro
       handleClose(); // Fecha o modal
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,16 @@
 //import axios from 'axios'
-import ApiAxios from './ApiAxios'
+import ApiAxios from "./ApiAxios";
 
-export const getTasks = () => ApiAxios.get(`/`)
+export const getTasks = () => ApiAxios.get(`/`);
 
-export const createTasks = () => ApiAxios.post(`/`)
+export const createTasks = ( formData ) => ApiAxios.post(`/`, formData);
+console.log('passei pela api post')
 
-export const updateTasks = ({ id }) => ApiAxios.patch(`/${id}`)
+export const updateTasks = ({ id, data }) => {
+//   const newData = transformData(data);
+//   console.log("Passei na api update...", id, data);
+  return ApiAxios.patch(`/${id}/`, data);
+};
 
-export const deleteTasks = ({ id }) => ApiAxios.delete(`/${id}`)
-//console.log(getTasks)
+export const deleteTasks = ({ id }) => ApiAxios.delete(`/${id}/`);
+// console.log('passei pela api delete')
