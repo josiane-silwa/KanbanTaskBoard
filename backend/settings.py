9aa60dc8b2cb16cd62bf0d005f038adddf087c5d
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2026-07-17 20:46:08 -0300

    atualizacao nas variaveis de ambientes

--- backend/settings.py
+++ backend/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j^_1k7=^ms(49y2ac5&&c%ot7k_!b5tv13%2q-183umol*7x9x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1'] 
