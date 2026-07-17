352f34767b91cbb60f16e6a1655fda19cd0fd201
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2026-07-17 20:27:58 -0300

    atualizacao no arquivo settings

--- backend/settings.py
+++ backend/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = 'django-insecure-j^_1k7=^ms(49y2ac5&&c%ot7k_!b5tv13%2q-183umol*7x9x
 # DEBUG = True
 DEBUG = False
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1'] 
-ALLOWED_HOSTS = ['kanbantaskboard.onrender.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['kanbantaskboard.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
