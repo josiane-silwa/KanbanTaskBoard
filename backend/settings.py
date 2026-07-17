231b8a3252ee3d218cad8e129ddbb762275e2833
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2026-07-17 20:57:10 -0300

    atualizacao nas variaveis de ambientes allowed

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1'] 
-ALLOWED_HOSTS = ['kanbantaskboard.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['kanbantaskboard.onrender.com', 'localhost', '127.0.0.1:8000']
 
 # Application definition
 INSTALLED_APPS = [
