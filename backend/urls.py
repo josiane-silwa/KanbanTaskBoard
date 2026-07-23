4055490432a014b1425abc139f57feba93b21a56
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2026-07-22 21:59:05 -0300

    Alteracao no redirecionamentoo para rota raiz

--- backend/urls.py
+++ backend/urls.py
@@ -19,11 +19,11 @@ from django.urls import path, include
 from django.http import JsonResponse
 
 # Função simples para responder na raiz (/)
-def home(request):
-    return JsonResponse({"message": "API Kanban TaskBoard em execução!"})
+# def home(request):
+#     return JsonResponse({"message": "API Kanban TaskBoard em execução!"})
 
 urlpatterns = [
-    path('', home),  # Adiciona essa linha para a raiz responder 200 OK
+    path('',  include('api.urls')),  # Adiciona essa linha para a raiz responder 200 OK
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')), # Prefixo de todas as rotas da API
 ]
