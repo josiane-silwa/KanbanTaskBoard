358b6df97fc7de10d61af36a55b7716318fe0513
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2026-07-22 21:29:11 -0300

    Ajustando o endpoint da api e da chamada da api no frontend

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
 
 router = DefaultRouter()
-router.register(r'api', TaskViewSet)
+#router.register(r'api', TaskViewSet)
+router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -22,7 +22,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://kanbantaskboard.onrender.com/api/api/",
+    "https://kanbantaskboard.onrender.com",
 ]
 
 STORAGES = {
