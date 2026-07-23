2edf1cd41b6a7af972f0a03d3737681cbc69cf2e
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2026-07-22 23:57:25 -0300

    Configuracao do frontend para deploy no render

--- frontend/src/services/ApiAxios.js
+++ frontend/src/services/ApiAxios.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
+const isDevelopment = import.meta.env.MODE === 'development'
+const myBaseUrl = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY
+
+// export default axios.create({
+//     baseURL:'http://127.0.0.1:8000/api/tasks/'
+// })
 export default axios.create({
-    baseURL:'http://127.0.0.1:8000/api/tasks/'
+    baseURL: myBaseUrl
 })
